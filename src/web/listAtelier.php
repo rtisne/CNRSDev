f7b379035e711d41f3ccd975643c990416c83ba6
Chaidou Chaharani Abdoussalami <chachaharaniab@emi.u-bordeaux1.fr>
2016-10-04 21:39:55 +0200

    Lister les élément de la BDD et ajout des bouton Modifier et supprimer Terminer manque juste les liens sur bouton

--- src/web/listAtelier.php
+++ src/web/listAtelier.php
@@ -1,4 +1,63 @@
 <?php
 include("header.template.php");
 include("footer.template.php");
+
+include("connexion.php");
+
+
+$reponse=$db->query("SELECT id, titre , etat , theme , type , lieu , duree , capacite , resume FROM ateliers ORDER BY id ASC");
+$Nbrligne = 0;
+
+echo '<table border="1">';
+while ($donnees=$reponse->fetch()){
+echo '<tr>';
+
+echo '<td>';
+echo $donnees["id"] ;
+echo '</td>';
+
+echo '<td>';
+echo $donnees["titre"];
+echo '</td>';
+
+echo '<td>';
+echo $donnees["etat"] ;
+echo '</td>';
+
+echo '<td>';
+echo $donnees["theme"] ;
+echo '</td>';
+
+echo '<td>';
+echo $donnees["type"] ;
+echo '</td>';
+
+echo '<td>';
+echo $donnees["lieu"] ;
+echo '</td>';
+
+echo '<td>';
+echo $donnees["duree"] ;
+echo '</td>';
+
+echo '<td>';
+echo $donnees["capacite"] ;
+echo '</td>';
+
+echo '<td>';
+echo $donnees["resume"] ;
+echo '</td>';
+
+echo '<td>';
+echo '<input type="submit" value="Modifier" target="updateAtelier.php">';
+echo '</td>';
+
+echo '<td>';
+echo '<input type="submit" value="Supprimer" href="">';
+echo '</td>';
+
+echo '</tr>';
+}
+echo '</table>';
+
 ?>
