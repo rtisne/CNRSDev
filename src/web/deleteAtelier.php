47803e69a0ea8df1f7b0b362b1522a196ba9dd1a
Romain <romain@MacBook-Pro-de-Romain.local>
2016-10-05 16:50:34 +0200

    correction suppression atelier

--- src/web/deleteAtelier.php
+++ src/web/deleteAtelier.php
@@ -1,17 +1,17 @@
 <?php
 include_once "connexion.php";
 
-if(isset($_GET['id'])){
-	
-	$id_atelier = $_GET['id'];
+if(isset($_GET['postId'])){
 
-    	$count = $db->exec('DELETE FROM ateliers WHERE id = '.$id_atelier);
- 	
+	$id_atelier = $_GET['postId'];
+
+    $count = $db->exec('DELETE FROM ateliers WHERE id = '.$id_atelier);
+
 	if($count > 0){
 		header('Location: listAtelier.php');
 	}else{
 		print("Aucun atelier à supprimer.\n");
 		header('Location: listAtelier.php');
-	}   	
+	}
 }
 ?>
