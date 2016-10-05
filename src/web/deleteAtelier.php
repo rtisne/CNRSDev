ea9c3fe7278e7f031e1c991de06a7af4db6d9dae
Martha Lopez Farfan <mlfarfan@emi.u-bordeaux1.fr>
2016-10-05 16:15:31 +0200

    tâche 6 done

---
+++ src/web/deleteAtelier.php
@@ -0,0 +1,17 @@
+<?php
+include_once "connexion.php";
+
+if(isset($_GET['id'])){
+	
+	$id_atelier = $_GET['id'];
+
+    	$count = $db->exec('DELETE FROM ateliers WHERE id = '.$id_atelier);
+ 	
+	if($count > 0){
+		header('Location: listAtelier.php');
+	}else{
+		print("Aucun atelier à supprimer.\n");
+		header('Location: listAtelier.php');
+	}   	
+}
+?>
