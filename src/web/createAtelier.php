96fe6c698d7c77639a59481106f40e9fe7b4c816
Chaidou Chaharani Abdoussalami <chachaharaniab@emi.u-bordeaux1.fr>
2016-10-05 21:34:23 +0200

    Tache 4 : Ajout dans la base de donnée sans vérification de l'élément dans la base

---
+++ src/web/createAtelier.php
@@ -0,0 +1,22 @@
+<?php
+include("header.template.php");
+include("form.template.php");
+include("footer.template.php");
+include("connexion.php");
+
+
+
+
+$req = $db->prepare('INSERT INTO ateliers(titre,theme,type,lieu,duree,capacite,animateur,resume) VALUES(:titre, :theme, :type, :lieu, :duree, :capacite, :animateur, :resume)');
+
+$req->execute(array(
+	'titre' => $_POST['titre'],
+	'theme' => $_POST['theme'],
+	'type' => $_POST['type'],
+	'lieu' => $_POST['lieu'],
+	'duree' => $_POST['duree'],
+	'capacite' => $_POST['capacite'],
+	'animateur' => $_POST['animateur'],
+	'resume' => $_POST['resume']
+	));
+?>
